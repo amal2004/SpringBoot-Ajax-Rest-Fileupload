62bb3875c048d7bf4b4468b5ea85f3a5a2863892
Amal <Amal@Amal-PC>
2017-11-13 15:58:04 -0500

    Final commit

---
+++ src/main/java/com/ajax/rest/fileupload/SpringBootAjaxRestFileuploadApplication.java
@@ -0,0 +1,12 @@
+package com.ajax.rest.fileupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAjaxRestFileuploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAjaxRestFileuploadApplication.class, args);
+	}
+}
---
+++ src/test/java/ajaxrestfileupload/SpringBootAjaxRestFileuploadApplicationTests.java
@@ -0,0 +1,16 @@
+package ajaxrestfileupload;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootAjaxRestFileuploadApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
